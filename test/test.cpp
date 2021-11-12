c61269712e9009aa1a7932dff6b4c09d37e8c716
xiong liu <flatflash@163.com>
2021-11-12 18:28:57 +0800

    1

--- test/test.cpp
+++ test/test.cpp
@@ -2,29 +2,32 @@
 
 using namespace std;
 
-class Base1 {
- public:
-  Base1() : value(10) {}
-  virtual ~Base1() {}
-  void test1() { cout << "Base test1..." << endl; }
+class Base1
+{
+  public:
+    Base1() : value(10) {}
+    virtual ~Base1() {}
+    void test1() { cout << "Base test1..." << endl; }
 
- protected:  // 保护
-  int value;
+  protected:  // 保护
+    int value;
 };
 // 默认为私有继承
-class Derived1 : Base1 {
- public:
-  void test2() { cout << "value is " << value << endl; }
+class Derived1 : Base1
+{
+  public:
+    void test2() { cout << "value is " << value << endl; }
 };
 
-class Base {
- public:
-  Base() : value(20) {}
-  virtual ~Base() {}
-  void test1() { cout << "Base test1..." << endl; }
+class Base
+{
+  public:
+    Base() : value(20) {}
+    virtual ~Base() {}
+    void test1() { cout << "Base test1..." << endl; }
 
- private:  //私有
-  int value;
+  private:  //私有
+    int value;
 };
 
 /**
@@ -32,17 +35,19 @@ class Base {
  * “子类可以访问父类的public和protected成员，不可以访问父类的private成员”——这句话对任何一种继承都是成立的。
  *
  */
-class Derived : Base {
- public:
-  using Base::value;
-  void test2() { cout << "value is " << value << endl; }
+class Derived : Base
+{
+  public:
+    using Base::value;
+    void test2() { cout << "value is " << value << endl; }
 };
 
-int main() {
-  Derived1 d1;
-  d1.test2();
+int main()
+{
+    Derived1 d1;
+    d1.test2();
 
-  Derived d;
-  d.test2();
-  return 0;
+    Derived d;
+    d.test2();
+    return 0;
 }
